1812fd9e815987b9a0aa97ce9a3207d97c7ce2db
HP <20060339@stu.omu.edu.tr>
2024-01-07 17:14:00 +0300

    edit

--- lib/home.dart
+++ lib/home.dart
@@ -11,7 +11,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
         child: Text('Home Screen  aaaaaaaaaa'),
       ),
